747e4fb65d00fd6e15c5e662370cf46430997d76
shivcodecf <shivam2113128@akgec.ac.in>
2025-10-31 20:00:15 +0530

    vercel 404 error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // client/src/services/api.js
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE = import.meta.env.VITE_API_BASE
 
 export const api = axios.create({
   baseURL: API_BASE,
